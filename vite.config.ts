6d0f7345a04020eef96976723a1745729135f0fc
Gagan vishwakarma <gaganvishwakarma46@gmail.com>
2023-12-23 14:56:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/User-Login-React-App"
+  base: "/User-Login-app-using-Auth0"
 })
